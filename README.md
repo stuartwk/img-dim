13e848e304e024582c14223a183696f6ec686a11
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2016-07-19 22:31:14 -0400

    add classes to parent based on child image dimensions

---
+++ README.md
@@ -0,0 +1,8 @@
+# Add Class to Parent Depending on Image Dimensions
+
+Allows you to add classes to html elements with a child images depending on the dimensions of the image. This way, the user can upload long or wide images and classes will be added so you can shift your layout.
+
+1. Add the class 'dim-par' to the parent element you want to add dynamic class.
+2. Add the class 'dim-check' to the child <img> element
+
+If the image is long, 'dim-par--long' class will be added to the parent element. If the image is wide, 'dim-par--wide' class will be added.  
---
+++ scripts.js
@@ -0,0 +1,30 @@
+function getDim() {
+
+	var x = 0,
+			y = 0,
+			parents = [];
+
+	Array.from(document.getElementsByClassName('dim-check')).forEach(function(dimImg){
+		parents[y] = dimImg.closest('.dim-par');
+
+		var img = new Image();
+		img.load_order = x;
+		img.onload = function() {
+			var dim_width = this.width;
+			var dim_height = this.height;
+			var width_to_height = dim_width / dim_height * 100;
+			var target_parent = parents[this.load_order];
+
+			if( width_to_height < 75 ){
+				target_parent.className += ' dim-par--long';
+			} else {
+				target_parent.className += ' dim-par--wide';
+			}
+		}
+		img.src = dimImg.src;
+		x++; y++;
+	});
+
+}
+
+getDim();
